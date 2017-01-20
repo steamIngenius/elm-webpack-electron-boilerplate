abc5237858eda372c1f38b04b8acd27fb9499429
Micah Hawkins <micahbhawkins@gmail.com>
2017-01-19 16:20:33 -0800

    adds a basic counter example, everything seems to be working

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,55 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text, div, h2, button)
+import Html.Events exposing (onClick)
+
+
+-- Model
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , h2 [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Decrement ->
+            model - 1
+
+        Increment ->
+            model + 1
+
+
+
+-- Main entry
 
 
 main =
-    text "And stuff!"
+    Html.beginnerProgram
+        { model = 0
+        , view = view
+        , update = update
+        }
