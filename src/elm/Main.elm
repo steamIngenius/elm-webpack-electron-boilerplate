84cc4fa8d3bb422b09653cd42000523ca662f447
Micah Hawkins <micahbhawkins@gmail.com>
2017-01-20 13:20:27 -0800

    javascript interop with crypto rollup using ports

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,19 +1,38 @@
-module Main exposing (..)
+port module Main exposing (..)
 
-import Html exposing (Html, text, div, h2, button)
-import Html.Events exposing (onClick)
+import Html exposing (Html, text, div, h2, button, input, form, label)
+import Html.Attributes exposing (type_, style, placeholder, value)
+import Html.Events exposing (onSubmit, onInput)
+import Http
 
 
 -- Model
 
 
 type alias Model =
-    Int
+    { username : String
+    , password : String
+    , server : String
+    , serverPort : String
+    , sessionId : Maybe String
+    , error : Maybe String
+    }
 
 
-type Msg
-    = Increment
-    | Decrement
+initModel : Model
+initModel =
+    Model
+        ""
+        ""
+        ""
+        ""
+        Nothing
+        Nothing
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
 
 
 
@@ -22,10 +41,36 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , h2 [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
+    form
+        [ onSubmit Login
+        , style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "column" )
+            , ( "align-items", "center" )
+            ]
+        ]
+        [ input
+            [ onInput SetServer
+            , placeholder "[host]"
+            ]
+            []
+        , input
+            [ onInput SetPort
+            , placeholder "[port]"
+            ]
+            []
+        , input
+            [ onInput SetUsername
+            , placeholder "Username"
+            ]
+            []
+        , input
+            [ onInput SetPassword
+            , type_ "password"
+            , placeholder "Password"
+            ]
+            []
+        , button [ type_ "Submit" ] [ text "Login" ]
         ]
 
 
@@ -33,23 +78,89 @@ view model =
 -- Update
 
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Decrement ->
-            model - 1
+type Msg
+    = Login
+    | SetUsername String
+    | SetPassword String
+    | SetServer String
+    | SetPort String
+    | Encrypt String
+    | LoginResponse (Result Http.Error String)
 
-        Increment ->
-            model + 1
+
+port encrypt : String -> Cmd msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        _ =
+            Debug.log "updating state..." ( msg, model )
+    in
+        case msg of
+            Login ->
+                ( model, encrypt model.password )
+
+            SetUsername input ->
+                ( { model
+                    | username = input
+                  }
+                , Cmd.none
+                )
+
+            SetPassword input ->
+                ( { model
+                    | password = input
+                  }
+                , Cmd.none
+                )
+
+            SetPort input ->
+                ( { model
+                    | serverPort = input
+                  }
+                , Cmd.none
+                )
+
+            SetServer input ->
+                ( { model
+                    | server = input
+                  }
+                , Cmd.none
+                )
+
+            Encrypt ciphertext ->
+                let
+                    _ =
+                        Debug.log "Got something back from js: " ciphertext
+                in
+                    ( model, Cmd.none )
+
+            _ ->
+                ( model, Cmd.none )
+
+
+
+-- Susbcriptions
+
+
+subs : Model -> Sub Msg
+subs model =
+    encrypted Encrypt
+
+
+port encrypted : (String -> msg) -> Sub msg
 
 
 
 -- Main entry
 
 
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = 0
+    Html.program
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subs
         }
--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,15 @@
 var Elm = require('../elm/Main');
+var crypto = require('./utils/crypto');
 var container = document.getElementById('container');
 var app = Elm.Main.embed(container);
+
+app.ports.encrypt.subscribe(function(plaintext) {
+  let key = new crypto();
+  const modulusBase16 = '9e60bec68ea009bd73f4e8338c232a9431af888bf3c5781e287a9fd0ccc9615e94a0e53391657e17488017c1646c53da9223602308a7edad80100c2e72040878c5381d09f7d9c4e4d7071090daf25ec0440f4c95386f6ab185e01aa3ce09df861cdd89fd61f71d628e05e8857f9d6dfe5d2b9e31f79b8a0961d2117322b98cb4ad8b13d97e937646a509b88fb56e1add0e90bbaf18454d68508e25100dee9e0a6aa623b99aeeabaf88963d3ed32f48bf623d486fe1c675f7d17d40d9614eddac42fd28c9f0c537cb08f56e2e35e276c04548661a455deba7c60ffb2f03286545c501be9c322dc0198ec27df190e794c7f84a4a6ce01023fcd8d5ce13c4648fc5';
+  const exponent = '65537';
+
+  key.setPublic(modulusBase16, Number(exponent).toString(16));
+  const hardPass = key.encrypt_b64(plaintext);
+
+  app.ports.encrypted.send(hardPass);
+})
